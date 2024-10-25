f5c365e1a4326c85f19ceea5da1db411168a4563
Alfred-Jijo <alfredjijo28@gmail.com>
2024-10-25 19:17:01 +0100

    make paints render logic

--- src/main.c
+++ src/main.c
@@ -4,20 +4,53 @@
 
 #include <typedef.h>
 
-
+#define COLOUR_COUNT 26
 
 int main(void) {
-    i16 width = GetScreenWidth();
-    i16 height = GetScreenHeight();
+    u16 width = GetScreenWidth();
+    u16 height = GetScreenHeight();
     const static char *title = "Physics Simulation";
 
     fprintf(stdout, "Project Initialising");
 
-    SetTargetFPS(30);
+    Color colours[COLOUR_COUNT] = {
+        LIGHTGRAY, GRAY, DARKGRAY, YELLOW, GOLD, ORANGE, PINK, RED, MAROON,
+        GREEN, LIME, DARKGREEN, SKYBLUE, BLUE, DARKBLUE, PURPLE, VIOLET,
+        DARKPURPLE, BEIGE, BROWN, DARKBROWN, WHITE, BLACK, BLANK, MAGENTA, RAYWHITE
+    };
+
+    Rectangle colourRects[COLOUR_COUNT] = {0};
+
+    for (u8 i = 0; i < COLOUR_COUNT; i++) {
+        colourRects[i].x = 10 + 30.f*i + 2*i;
+        colourRects[i].y = 10;
+        colourRects[i].width = 30;
+        colourRects[i].height = 30;
+    }
+
+    int colorSelected = 0;
+    int colorSelectedPrev = colorSelected;
+    int colorMouseHover = 0;
+    float brushSize = 20.0f;
+    bool mouseWasPressed = false;
+
+    Rectangle buttonSaveRec = { 750, 10, 40, 30 };
+    bool buttonSaveMouseHover = false;
+    bool showSaveMessage = false;
+    int saveMessageCounter = 0;
+
+    RenderTexture2D canvas = LoadRenderTexture(width, height);
+
+    BeginTextureMode(canvas);
+    ClearBackground(RAYWHITE);
+    EndTextureMode();
+
+    SetTargetFPS(60);
     InitWindow(width, height, title);
     while (!WindowShouldClose()) {
         BeginDrawing();
-            ClearBackground(BLACK);
+            f32 mouseCircleRadius = 2.0f;
+            DrawCircle(GetMousePosition().x,GetMousePosition().y,mouseCircleRadius,WHITE);
         EndDrawing();
     }
     CloseWindow();
