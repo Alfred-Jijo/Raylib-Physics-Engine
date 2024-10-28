c5ba6fbbaec9403fd709f55920f1f3e0fcbbd7c6
Alfred-Jijo <alfredjijo28@gmail.com>
2024-10-28 12:13:52 +0000

    changes to FPS and dot_quality s

--- src/main.c
+++ src/main.c
@@ -5,30 +5,27 @@
 /// Gravity in a Bounding Box.                                               ///
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
 #include "typedef.h"
 
-#include <raylib.h>
-#include <time.h>
-
+#include "raylib.h"
 #include "raymath.h"
 
-typedef Vector2 Vec2;
 typedef struct Dot {
-    Vec2 position;
-    Vec2 velocity;
+    Vector2 position;
+    Vector2 velocity;
 } dot_point;
 
 int main(void) {
-    srand(time(0));
-    const double gravity_force = 9.81f;
-    const double gravity_point_speed = 1.2f;
-    const double speed_limit = 100.1f;
+    const double gravity_force = 5000000;
+    const double gravity_point_speed = 50;
+    const double speed_limit = 7500;
 
-    const u16 screen_width = 1080;
-    const u16 screen_height = 720;
+    const u16 screen_width = 1440;
+    const u16 screen_height = 960;
     const char* screen_title = "Physics Simulation";
 
     InitWindow(screen_width, screen_height, screen_title);
@@ -37,27 +34,31 @@ int main(void) {
     u8 show_general_details = 1;
     u8 show_gravity_details = 1;
     u8 use_mouse_as_gravity_point = 0;
+    u8 use_dark_mode = 0;
 
     // Gravity Calc vars
-    Vec2 gravity_point = (Vec2) { screen_width/ 2, screen_height / 2 };
-    Vec2 scaled_gravity_point;
-    Vec2 move;
+    Vector2 gravity_point = (Vector2) { screen_width/ 2, screen_height / 2 };
+    Vector2 scaled_gravity_point;
+    Vector2 move;
     double distance;
-    double gravity_direction = 0f;
+    double gravity_direction = 0;
 
     // Dots vars
-    size_t dot_quantity = 100;
+    size_t dot_quantity = 2000;
     dot_point *dot_array = (dot_point *)malloc(dot_quantity * sizeof(dot_point));
 
     for (size_t i = 0; i < dot_quantity; i++) {
-        dot_array[i].position = (Vec2) {
+        dot_array[i].position = (Vector2) {
             (float)(rand() % screen_width) * 100,
             (float)(rand() % screen_height) * 100
         };
-        dot_array[i].velocity = Vector2Zero;
+        dot_array[i].velocity = (Vector2) {
+                                    .x = 0,
+                                    .y = 0
+                                };
     }
 
-    SetTargetFPS(120);
+    SetTargetFPS(130);
 
     // Main Sim Loop
     while (!WindowShouldClose()) {
@@ -81,10 +82,15 @@ int main(void) {
             gravityDirection = 0;
         }
         */
+
         // Toggle if the gravity point is based on the mouse position or not
         if (IsKeyReleased(KEY_P)) {
             use_mouse_as_gravity_point = (u8)(use_mouse_as_gravity_point == 0);
-            gravity_point = (Vec2) {screen_width / 2, screen_height / 2};
+            gravity_point = (Vector2) {screen_width / 2, screen_height / 2};
+        }
+
+        if (IsKeyReleased(KEY_K)) {
+            use_dark_mode = (u8)(use_dark_mode == 0);
         }
 
         // Update Loop
@@ -111,9 +117,104 @@ int main(void) {
                 gravity_point.x = screen_width / 2;
             }
         }
+
+        for (size_t i = 0; i < dot_quantity; i++) {
+            scaled_gravity_point = Vector2Scale(gravity_point, 100);
+            distance = Vector2Distance(scaled_gravity_point, dot_array[i].position);
+
+            if (!(distance < 99
+                  && distance > -99)) {
+                move = Vector2MoveTowards(dot_array[i].position, scaled_gravity_point, 1);
+                // Now here's where it gets funky
+                dot_array[i].velocity = Vector2Add(
+                    Vector2Scale(
+                        Vector2Normalize(
+                            Vector2Subtract(
+                                move,
+                                dot_array[i].position)),
+                        (GetFrameTime() * gravity_force * gravity_direction) / (distance / 2)),
+                    dot_array[i].velocity);
+
+                if (Vector2Length(dot_array[i].velocity) > speed_limit) {
+                    dot_array[i].velocity = Vector2Scale(Vector2Normalize(dot_array[i].velocity), speed_limit);
+                }
+            }
+        }
+
+        // Update Position
+        for (size_t i = 0; i < dot_quantity; i++) {
+            dot_array[i].position = Vector2Add(dot_array[i].position, Vector2Scale(dot_array[i].velocity, GetFrameTime()));
+        }
+
+        // Drawing time
+        BeginDrawing();
+        if (use_dark_mode) {
+            ClearBackground(BLACK);
+            for (size_t i = 0; i < dot_quantity; i++) {
+                DrawCircleV(Vector2Scale(dot_array[i].position, 0.01), 2.0f, (Color) {(unsigned char)(Vector2Length
+                (dot_array[i].velocity) * 200 / speed_limit + 55), 0, 0, 255});
+            }
+            DrawText("DARK MODE: ON", screen_width - 140, 5, 18, ORANGE);
+        }
+        else {
+            ClearBackground(RAYWHITE);
+            for (size_t i = 0; i < dot_quantity; i++) {
+                DrawCircleV(Vector2Scale(dot_array[i].position, 0.01), 2.0f, BLACK);
+            }
+        }
+
+        DrawCircleV(gravity_point, 2.0f, GRAY);
+        if (show_general_details) {
+            // General details
+            char fps_string[1000];
+            snprintf(fps_string, sizeof(fps_string), "FPS: %d", GetFPS());
+
+            char dot_quantity_string[1000];
+            snprintf(dot_quantity_string, sizeof(dot_quantity_string), "DOTS: %d", dot_quantity);
+
+            DrawText(fps_string,5, 5, 18, ORANGE);
+            DrawText(dot_quantity_string,5, 25, 18, ORANGE);
+        }
+
+        if (show_gravity_details) {
+            char grav_force_string[1000];
+            char grav_mode_string[1000];
+            char grav_pos_string[1000];
+            char grav_x[1000];
+            char grav_y[1000];
+            char grav_point_speed_string[1000];
+
+            snprintf(grav_force_string, sizeof(grav_force_string), "GRAV FORCE: %d", gravity_force);
+            snprintf(grav_mode_string, sizeof(grav_mode_string), "GRAV. MODE: %s",
+                     (gravity_direction == 0) ? "DISABLED" :
+                     ((gravity_direction == 1) ? "NORMAL" : "REVERSED"));
+            snprintf(grav_pos_string, sizeof(grav_pos_string), "GRAV POSITION: %d", gravity_point);
+            snprintf(grav_x, sizeof(grav_x), "X: %d", gravity_point.x);
+            snprintf(grav_y, sizeof(grav_y), "Y: %d", gravity_point.y);
+            snprintf(grav_point_speed_string, sizeof(grav_point_speed_string), "GRAV POINT SPEED: %d", gravity_point_speed);
+
+            DrawText(grav_force_string, 5, 55, 18, ORANGE);
+            DrawText(grav_mode_string, 5, 75, 18, ORANGE);
+            DrawText(grav_pos_string, 5, 95, 18, ORANGE);
+            DrawText(grav_x, 5, 115, 18, ORANGE);
+            DrawText(grav_y, 5, 135, 18, ORANGE);
+
+            if(!use_mouse_as_gravity_point) {
+                DrawText(grav_point_speed_string, 5, 155, 18, ORANGE);
+            }
+        }
+        // KEYS
+        DrawText(
+        "INSERT - Info. 1 Toggle | DELETE - Info. 2 Toggle | R - Grav. Mode Toggle | P - Grav. Point Toggle | K - Toggle Dark Mode",
+            5,
+            screen_height - 20,
+            20,
+            ORANGE
+        );
+        EndDrawing();
     }
 
+    free(dot_array);
     CloseWindow();
-
     return EXIT_SUCCESS;
 }
